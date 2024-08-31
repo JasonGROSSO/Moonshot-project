8b9f048d6a88ea72c0f6877ecb54e7c4eb7a3b0d
JasonGROSSO <114397870+JasonGROSSO@users.noreply.github.com>
2024-09-01 00:09:56 +0200

    fix error #14
    inverted the 2 part of the if else

--- src/libs/input_file_handling.h
+++ src/libs/input_file_handling.h
@@ -36,13 +36,13 @@ char *read_file(char *filePath)
     // Start of part 2
     // Look at the last charactere of the path provided
     // If diffrent from "c" or "h" return an error (to be implemented)
-    if (last_char_path != 'c' | 'h')
+    if (last_char_path == 'c' || 'h')
     {
-        printf("file %s is not a c file \n", filePath);
+        printf("file %s is a c file \n", filePath);
     }
     else
     {
-        printf("file %s is a c file \n", filePath);
+        printf("file %s is not a c file \n", filePath);
     }
 
     // Start of part 3
@@ -62,7 +62,7 @@ char *read_file(char *filePath)
     }
     else
     {
-        printf("memory allocation succesfull \n");
+        printf("memory allocation successfull \n");
     }
 
     // assign the content of the file to the buffer
