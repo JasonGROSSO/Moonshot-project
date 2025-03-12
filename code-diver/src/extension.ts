df2af1af9d24b5cefbbdb57c8958a33c98c3adec
JasonGROSSO <114397870+JasonGROSSO@users.noreply.github.com>
2025-03-12 19:06:01 +0100

    housekeeping plus empty sidebar (dataprovider registration issue)

--- code-diver/src/extension.ts
+++ code-diver/src/extension.ts
@@ -1,41 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
 import * as vscode from 'vscode';
-import { SidebarViewProvider } from './sidebar';
+import { MySidebarProvider } from './sidebar';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Console diagnostic information (console.log) and errors (console.error)
-	// Will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-diver" is active!');
-
-	const provider = new SidebarViewProvider(context.extensionUri);
-
+	console.log("Code-Diver extension activated");
+	const provider = new MySidebarProvider(context.extensionUri);
 	context.subscriptions.push(
-		vscode.window.registerWebviewViewProvider(
-			SidebarViewProvider.viewType,
-			provider
-		)
+	  vscode.window.registerWebviewViewProvider(MySidebarProvider.viewType, provider)
 	);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand("vscodeSidebar.menu.view", () => {
-			const message = "Menu/Title of extension is clicked !";
-			vscode.window.showInformationMessage(message);
-		})
-	);
-
-	// Command has been defined in the package.json file
-	// Provide the implementation of the command with registerCommand
-	// CommandId parameter must match the command field in package.json
-	let openWebView = vscode.commands.registerCommand('vscodeSidebar.openview', () => {
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Command " Sidebar View [vscodeSidebar.openview] " called.');
-	});
-
-	context.subscriptions.push(openWebView);
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() { }
+  }
--- code-diver/src/sidebar.ts
+++ code-diver/src/sidebar.ts
@@ -1,54 +1,40 @@
-import * as vscode from "vscode";
-import * as fs from 'fs';
+import * as vscode from 'vscode';
 
-export class SidebarViewProvider implements vscode.WebviewViewProvider {
-  public static readonly viewType = "vscodeSidebar.openview";
-
+export class MySidebarProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = 'mySidebarView';
   private _view?: vscode.WebviewView;
 
   constructor(private readonly _extensionUri: vscode.Uri) {}
 
-  resolveWebviewView(
+  public resolveWebviewView(
     webviewView: vscode.WebviewView,
-    context: vscode.WebviewViewResolveContext<unknown>,
+    context: vscode.WebviewViewResolveContext,
     token: vscode.CancellationToken
-  ): void | Thenable<void> {
+  ) {
     this._view = webviewView;
 
     webviewView.webview.options = {
-      // Allow scripts in the webview
       enableScripts: true,
-      localResourceRoots: [this._extensionUri],
+      localResourceRoots: [this._extensionUri]
     };
-    webviewView.webview.html = this.getHtmlContent(webviewView.webview);
+
+    webviewView.webview.html = this.getHtmlForWebview(webviewView.webview);
   }
 
-  private getHtmlContent(webview: vscode.Webview): string {
-    const htmlPath = vscode.Uri.joinPath(this._extensionUri, "assets", "index.html");
-    const htmlContent = fs.readFileSync(htmlPath.fsPath, 'utf8');
-
-    const scriptUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "assets", "main.js")
-    );
-    const stylesheetUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "assets", "main.css")
-    );
-
-    const nonce = getNonce();
-
-    return htmlContent
-      .replace('${scriptUri}', scriptUri.toString())
-      .replace('${stylesheetUri}', stylesheetUri.toString())
-      .replace('${nonce}', nonce);
+  private getHtmlForWebview(webview: vscode.Webview): string {
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Sidebar</title>
+      </head>
+      <body>
+        <h1>Welcome to My Sidebar!</h1>
+        <p>This is custom content in your sidebar.</p>
+      </body>
+      </html>
+    `;
   }
-}
-
-function getNonce() {
-  let text = "";
-  const possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < 32; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
 }
