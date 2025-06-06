89b24e02ea721a91303de40e1538f999ad0c4403
JasonGROSSO <114397870+JasonGROSSO@users.noreply.github.com>
2025-06-06 10:24:07 +0200

    changed to command line based

--- code-diver/extension-src/extension.ts
+++ code-diver/extension-src/extension.ts
@@ -1,33 +1,15 @@
 import * as vscode from 'vscode';
-import { CodeDiverSidebarViewProvider } from './sidebar';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log("Code-Diver extension activated");
+    console.log("Code-Diver extension activated");
 
-	const provider = new CodeDiverSidebarViewProvider(context.extensionUri);
-
-	// Display the sidebar view
-	context.subscriptions.push(
-		vscode.window.registerWebviewViewProvider(
-			CodeDiverSidebarViewProvider.viewType,
-			provider
-		)
-	);
-
-	// Display a button in the header of the sidebar view
-	context.subscriptions.push(
-		vscode.commands.registerCommand('code-diver.menu.close', () => {
-			const message = "Close Button is clicked";
-			vscode.window.showInformationMessage(message);
-		})
-	);
-
-	let openWebView = vscode.commands.registerCommand('code-diver.openview', () => {
-		vscode.window.showInformationMessage('Command " Sidebar View [code-diver.openview] " called.');
-	});
-
-	context.subscriptions.push(openWebView);
-
+    const runMainCommand = vscode.commands.registerCommand('code-diver.startDive', () => {
+        const terminal = vscode.window.createTerminal({ name: 'Code Diver Main' });
+        terminal.show();
+        terminal.sendText('npx ts-node ./interpreter-src/main.ts');
+        vscode.window.showInformationMessage('Running main.ts in terminal...');
+    });
+    context.subscriptions.push(runMainCommand);
 }
 
 export function deactivate() {
